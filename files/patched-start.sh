77227aeccecf3a493f0dd6fc7bd22d9b964f5989
Fai Yip <fai@mugetsu.co.uk>
2018-08-13 02:00:10 +0100

    Start celery worker on boot

--- files/patched-start.sh
+++ files/patched-start.sh
@@ -4,4 +4,7 @@
 sed -i "s/IP_ADDRESS = '[^']*'/IP_ADDRESS = '${RC_HOOKS_HOST:-$DEFAULT_RC_HOOKS_HOST}'/g" \
 	~/.rccontrol/community-1/profile/lib/python2.7/site-packages/rhodecode/lib/hooks_daemon.py
 
+# start the celery worker
+.rccontrol/community-1/profile/bin/celery worker --beat --app rhodecode.lib.celerylib.loader --loglevel DEBUG --ini=/home/rhodecode/.rccontrol/community-1/rhodecode.ini &
+
 exec bash ./start.sh
